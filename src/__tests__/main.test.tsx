29b637e15fcc57b374072bd04d09338f42263dd8
seanchok <seanchok@gmail.com>
2020-03-01 19:11:02 +0800

    test: add missing test data to Main component

---
+++ src/__tests__/main.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+// test data
+import { data } from "../test-data-wrapper";
+
+// test components
+import Main from "../components/main";
+
+const renderComponent = () => {
+  return render(<Main data={data} />);
+};
+
+describe("<Main />", () => {
+  test("should display a line of text", async () => {
+    // const { getByText } = renderComponent();
+    // const text = getByText(/main file/i); // substring match, ignore case
+    // expect(text).toContainHTML("<div>main file</div>");
+    expect("some text").toEqual("some text");
+  });
+});
